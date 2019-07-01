7d41d44beb6d51445ac27c9d90cc84adb7ac03cb
Tung Zie Yeong <ohqoo123@gmail.com>
2019-07-02 02:18:30 +0800

    Adding flying enemy vision and change throw scene to testing for the flying enemy

---
+++ Assets/Scripts/FlyingEnemyVision.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingEnemyVision : MonoBehaviour
+{
+    private bool isFlipping;
+
+    private void Start()
+    {
+        isFlipping = false;
+        StartCoroutine("Flip");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // if enter lose?
+        if (other.gameObject.name == "Player")
+        {
+            Debug.Log("Spotted");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // this jus for checking working or not
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("Searching");
+        }
+    }
+
+    private IEnumerator Flip()
+    {
+        while (true)
+        {
+            // for now i only able to flip the sprite every 3 sec because dk how to implement the code for rotating the vision every 3 sec
+            GetComponent<SpriteRenderer>().flipX = isFlipping;
+            isFlipping = !isFlipping;
+            yield return new WaitForSeconds(3f);
+        }
+    }
+}
