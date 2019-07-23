6e928c33e50aa975bb11844e52e0d74b809c357e
Kenxaz <ribyjoey@hotmail.com>
2019-07-24 05:10:22 +0800

    Added PauseMenu Art, DeathMenu Art

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, Vector2.one);
+    }
+
+
+    public void TriggerDialogue()
+    {
+        //FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    public void endTriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().EndDialogue();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(Global.tagPlayer))
+        {
+
+            TriggerDialogue();
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(Global.tagPlayer))
+        {
+
+            endTriggerDialogue();
+
+        }
+    }
+}
