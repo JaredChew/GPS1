3d0354b2828a9f2d5fc054265b3366f0b67bdfd3
Kenxaz <ribyjoey@hotmail.com>
2019-08-08 23:42:07 +0800

    Added Cutscenes

---
+++ Assets/Scripts/Cutscene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene : MonoBehaviour
+{
+    private float startTime = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        startTime += 0.1f * Time.deltaTime;
+
+        if(startTime >= 1.0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/Cutscene2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutscene2 : MonoBehaviour
+{
+    private float startTime = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        startTime += 0.1f * Time.deltaTime;
+
+        if (startTime >= 2.0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
